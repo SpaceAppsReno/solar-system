fadf0522a481ac75219ea92b90d12a23774c3028
Christopher Baker <christopher@hmudesign.com>
2015-04-12 16:27:37 -0700

    beautify orbital_mechanics

--- orbital_mechanics/objects.js
+++ orbital_mechanics/objects.js
@@ -158,5 +158,18 @@ var objects = (function() {
 		}
 	];
 	
+	function slugify(name) {
+		return name.toLowerCase()
+			.replace(/\s+/g, '-')           // Replace spaces with -
+			.replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+			.replace(/\-\-+/g, '-')         // Replace multiple - with single -
+			.replace(/^-+/, '')             // Trim - from start of text
+			.replace(/-+$/, '');            // Trim - from end of text
+	}
+	
+	objects.map(function(value) {
+		value.slug = slugify(value.name);
+	});
+	
 	return objects;
 })();
